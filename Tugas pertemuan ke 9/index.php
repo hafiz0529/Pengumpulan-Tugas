0aa47318bb1c8b6c9a201244098079519ee71e4f
Abdullah Hafiz <72722308+hafiz0529@users.noreply.github.com>
2023-04-05 00:22:09 +0700

    pengumpulan tugas ke 9

---
+++ Tugas pertemuan ke 9/index.php
@@ -0,0 +1,31 @@
+<?php
+//memanggil dan memproses file bagian atas
+include_once 'header.php';
+//memanggil dan memproses file bagian menu
+include_once 'menu.php';
+?>
+ <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid px-4">
+                        
+                        <?php 
+                        //algoritma menangkap url agar masuk kedalam index
+                        $url = $_GET['url'];
+                        if($url == 'dashboard'){
+                            include_once 'dashboard.php';
+                        } else if (!empty($url)){
+                            include_once ''.$url.'.php';
+                        } 
+                        else { 'dashboard.php';
+                        }
+                        
+                        
+                        ?>
+                    </div>
+                </main>
+</div>
+
+<?php
+//memanggil file bagian bawah
+include_once 'footer.php';
+?>
